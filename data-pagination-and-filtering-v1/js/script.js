a374fb072c3d2c4c1a9eb733a90129f23734cf2d
joelle_walley <jojowalley@berkeley.edu>
2021-03-10 15:58:10 -0800

    2nd commit and also fixed some of my code in showPage to hopefully make it work, that is definately tomorrows problem tho aha

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -19,31 +19,40 @@ This function will create and insert/append the elements needed to display a "pa
 */
 function showPage(list, page) {
    // create two variables which will represent the index for the first and last student on the page
-      //startIndex = (page parameter * items per page) - items per page
-      //endIndex = page parameter * items per page
+   let startIndex = ((page * 9) - 9);
+   let endIndex = page * 9;
    // select the element with a class of `student-list` and assign it to a variable
-      // studentList = document.querySelector('ul');
+   const studentList = document.querySelector('.student-list');
 
    // set the innerHTML property of the variable you just created to an empty string
-      //studentList.innerHTML = " ";
+   studentList.innerHTML = " ";
 
    // loop over the length of the `list` parameter
-         //for (let i = 0; i < studentList.length; i++) {
-            //if (i >= startIndex && i < endIndex) {
-               //var studentItem = document.createElement(" ");
-               //studentItem.innerHTML = '(inset text here)'
-               // temperate literal = `string text ${expression} string text`
-               //document.body.appendChild(studentItem);
-               //studentItem.insertAdjacentHTML('beforeend', '(insert text here)');
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+               //temperate literal = `string text ${expression} string text
+         let studentListIndexer = data[i];
+               // create the elements needed to display the student information
+         let studentItem = `<li class="student-item cf">
+    <div class="student-details">
+      <img class="avatar" src="${studentListIndexer.picture.large}" alt="Profile Picture">
+      <h3>${studentListIndexer.name.first} ${studentListIndexer.name.last}</h3>
+      <span class="email">${studentListIndexer.email}</span>
+    </div>
+    <div class="joined-details">
+      <span class="date"> ${studentListIndexer.registered.date}</span>
+    </div>
+  </li>`;
+               // insert the above elements = stored in studentItem 
+      studentList.insertAdjacentHTML('beforeend', studentItem);
 
-            //}
-
-         //}
-   // inside the loop create a conditional to display the proper students
-   // inside the conditional:
-   // create the elements needed to display the student information
-   // insert the above elements
+      }
+   }
 }
+
+// inside the loop create a conditional to display the proper students
+// inside the conditional:
+// create the elements needed to display the student information
 
 
 /*
@@ -53,29 +62,56 @@ This function will create and insert/append the elements needed for the paginati
 
 function addPagination(list) {
    // create a variable to calculate the number of pages needed
-      //numOfPages = Math.ceil(list.length / items per page);
-      
-
+   const numOfPages = Math.ceil(list.length / data.length);
    // select the element with a class of `link-list` and assign it to a variable
-      // const linkList = document.querySelector('ul');
+   let linkList = document.querySelector('.link-list');
    // set the innerHTML property of the variable you just created to an empty string
-      // linkList.innerHTML = '';
+   linkList.innerHTML = '';
    // loop over the number of pages needed
-      // for (let i = 1; i <= numOfPages; i--) {
-         
+   for (let i = 1; i <= Math.round(numOfPages); i++) {
+      //<li>
+      //<button type="button">1</button>
+ //</li >
+      //Insert the elements you have created to the 
+      //link-list variable you created earlier. 
+      //The insertAdjacentHTML 
+      //
 
-      //}
+   }
    // create the elements needed to display the pagination button
    // insert the above elements
 
    // give the first pagination button a class of "active"
 
    // create an event listener on the `link-list` element
+
    // if the click target is a button:
    // remove the "active" class from the previous button
    // add the active class to the clicked button
    // call the showPage function passing the `list` parameter and page to display as arguments
+   // showPage(list, page);
 }
+
+// @source https://www.w3schools.com/howto/howto_js_filter_lists.asp
+//function myFunction() {
+// Declare variables:
+// var input, filter, ul, li, a, i, txtValue;
+// input = document.getElementById('myInput');
+// filter = input.value.toUpperCase();
+// ul = document.getElementById("myUL");
+// li = ul.getElementsByTagName('li');
+
+//Loop through all list items, and hide those who don't match the search query
+// for (i = 0; i < li.length; i++) {
+//    a = li[i].getElementsByTagName("a")[0];
+//    txtValue = a.textContent || a.innerText;
+//    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//    } else {
+//       li[i].style.display = "none";
+//    }
+// }
+// }
 
 
 // Call functions
