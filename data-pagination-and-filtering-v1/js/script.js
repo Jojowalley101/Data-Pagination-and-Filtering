0a160c6650028c3cfefe405345f62eb57de38dd4
joelle_walley <jojowalley@berkeley.edu>
2021-03-14 20:38:18 -0700

    submitting now

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -1,6 +1,7 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+@author Joelle Walley
 */
 
 "use strict";
@@ -10,8 +11,6 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
-
 
 /*
 Create the `showPage` function
@@ -51,11 +50,6 @@ function showPage(list, page) {
       }
    }
 }
-// showPage(data, 2);
-// inside the loop create a conditional to display the proper students
-// inside the conditional:
-// create the elements needed to display the student information
-
 
 /*
 Create the `addPagination` function
@@ -89,51 +83,14 @@ function addPagination(list) {
          //console.log(classNameActive);
       }
    }
+   //Insert the elements you have created to the link-list variable you created earlier. 
    linkList.addEventListener('click', goToPage);
-  
-      //Insert the elements you have created to the 
-      //link-list variable you created earlier. 
-      //The insertAdjacentHTML 
-      //
 }
-//showPage(data, 1);
-//addPagination(data);
 
-   // create the elements needed to display the pagination button
-   // insert the above elements
-
-   // give the first pagination button a class of "active"
-
-   // create an event listener on the `link-list` element
-
-   // if the click target is a button:
-   // remove the "active" class from the previous button
-   // add the active class to the clicked button
-   // call the showPage function passing the `list` parameter and page to display as arguments
-   // showPage(list, page);
-
-// const search = document.getElementById('.student-search');
-// const submit = document.querySelector('.student-search button');
-
-/* Variable to store HTMLCollection of table cells that you'll search through
-   But the same basic technique can work with other collections, even an array of objects */
-
-// const studentList2 = document.querySelector('.student-list');
-// // Helpful log statements
-// console.log(search);
-// console.log(submit);
-// console.log(studentList2);
-
-// function searchForStudents(searchInput, data) {
-//    console.log(searchInput);
-//    console.log(data);
-//    for (let i = 0; i < data.length; i++) {
-//       data[i] = 'No results found';
-//       if (searchInput.value.length !== 0 && data[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
-//          data[i] = showPage(data, 1);
-//       }
-//    }
-// }
+/**
+ * searchbar function adapted from simple search warmup from project. 
+ * This function allows the user to search for students throughout the data.
+ */
 
 const header = document.querySelector('.header');
 header.innerHTML += `<label for="search" class="student-search">
@@ -173,39 +130,26 @@ function searchBarFunc(list) {
    }
 }
 
-
-
-
-
-
 /**
- * Event listeners for buttons - Invoke your search function in the body of the callbacks in the event listeners below
+ * Event listeners for buttons - 
+ * Invoke your search function in the body of the callbacks 
+ * in the event listeners below
  */
 
-// /* submit listener */
+/* submit listener */
 submitButton.addEventListener('click', (event) => {
    event.preventDefault();
    searchBarFunc(data);
    console.log('Submit button is functional!');
 });
 
-// //    // Helpful log statement to test function
-   
-// // /* submit listener */
+/* submit listener */
 searchInput.addEventListener('keyup', () => {
    searchBarFunc(data);
+   console.log('Keyup event on the Search input is functional!');
 });
-
-// //    // Invoke your search function here - Arguments: search, tableCells
-//    searchForStudents(searchInput, data);
-
-
-// //    // Helpful log statement to test function
-//    console.log('Keyup event on the Search input is functional!');
-// });
 
 // Call functions
 
 showPage(data, 1);
 addPagination(data);
-//searchForStudents(searchInput, data);
