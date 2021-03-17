df078379eaa7bb17c43060da27b1e709c48ec87f
joelle_walley <jojowalley@berkeley.edu>
2021-03-17 11:51:16 -0700

    fixed it

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -70,13 +70,13 @@ function addPagination(list) {
       </li >`;
       linkList.insertAdjacentHTML('beforeend', button);
    }
-   //let btn1 = document.querySelector('.link-list').firstChild;
+   let btn1 = document.querySelector('.link-list').firstChild;
    //btn1.className = 'active';
    function goToPage(event) {
       if (event.target.tagName == "BUTTON") {
-         //let classNameActive = document.querySelector('.active');
+         let classNameActive = document.querySelector('.active');
          //classNameActive.className = '';
-         event.target = 'active';
+         event.target.classNameActive = 'active';
          //console.log(event.target);
          showPage(list, event.target.textContent);
 
